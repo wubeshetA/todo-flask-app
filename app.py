00b7debe6daa564e082174b8387e155e0a235c10
wubshetA <wubeshet.a.y@gmail.com>
2022-05-24 00:53:41 +0200

    database model added

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:wube@localhost:5432/todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+db.create_all()
+class Todo(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f"""<Todos>
+                id:{Todo.id} 
+                content:{Todo.content},
+                completed: {Todo.completed} """
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
